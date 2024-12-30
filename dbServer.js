e3db1f48e278959d6f8ea1e065c9cdb19274fc1f
Geattt <kent.ma04@gmail.com>
2024-12-30 17:02:41 +0800

    try without database

--- dbServer.js
+++ dbServer.js
@@ -30,25 +30,25 @@ app.use('/assets', express.static('assets'));
 
 require("dotenv").config()
 
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
+const DB_HOST = 'bt8kulbdpgvgv320qtox-mysql.services.clever-cloud.com'
+const DB_USER = 'root'
+const DB_PASSWORD = 'admin'
+const DB_DATABASE = 'classroom_reservation'
 const DB_PORT = process.env.DB_PORT
 
-const db = mysql.createPool({
-   connectionLimit: 100,
-   host: DB_HOST,
-   user: DB_USER,
-   password: DB_PASSWORD,
-   database: DB_DATABASE,
-   port: DB_PORT
-})
+// const db = mysql.createPool({
+//    connectionLimit: 100,
+//    host: DB_HOST,
+//    user: DB_USER,
+//    password: DB_PASSWORD,
+//    database: DB_DATABASE,
+//    port: DB_PORT
+// })
 
-db.getConnection( (err, connection)=> {   
-   if (err) throw (err)
-   console.log ("DB connected successful: " + connection.threadId)
-})
+// db.getConnection( (err, connection)=> {   
+//    if (err) throw (err)
+//    console.log ("DB connected successful: " + connection.threadId)
+// })
 
 const port = process.env.PORT
 app.listen(port, ()=> console.log(`Server Started on port ${port}...`))
